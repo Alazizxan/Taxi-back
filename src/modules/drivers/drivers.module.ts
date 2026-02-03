4e62c43571e50987cd2c3eef6cfeb5623a68d65a
Al Azizxan <123247928+Alazizxan@users.noreply.github.com>
2026-02-03 13:23:19 +0500

    Add NotificationModule to DriversModule imports

--- src/modules/drivers/drivers.module.ts
+++ src/modules/drivers/drivers.module.ts
@@ -9,6 +9,7 @@ import { OrdersModule } from '../orders/orders.module';
 import { PricingModule } from '../pricing/pricing.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { CommissionModule } from '../commission/commision.module';
+import { NotificationModule } from '../notifications/notification.module';
 
 import { DriverCronService } from './driver-corn.service';
 
@@ -22,6 +23,7 @@ import { DriverCronService } from './driver-corn.service';
     WalletModule,
     CommissionModule,
     OrdersModule,
+    NotificationModule,
   ],
   providers: [
     DriversService,
